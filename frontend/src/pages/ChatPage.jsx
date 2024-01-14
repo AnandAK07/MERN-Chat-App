0ec40d5ea42cbfcbfe85497b2e0859a59f9d0482
AnandAK07 <anandkumark055@gmail.com>
2024-01-14 18:16:23 +0530

    backend

---
+++ frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+export const ChatPage = () => {
+    const [chats,setChats]=useState([])
+    
+    const fetchChats=async()=>{
+        const {data}=await axios.get(`/api/chat`)
+        // console.log(data)
+        setChats(data)
+    }
+    console.log(chats)
+
+    useEffect(()=>{
+        fetchChats();
+    },[])
+  return (
+    <div>
+          {chats.map((chat,id)=>(
+              <div key={chat._id}>{chat.chatName}</div>
+          ))}
+    </div>
+  )
+}
