9ff5be260135df2de3679b0a5a619a966b406671
AnandAK07 <anandkumark055@gmail.com>
2024-01-18 00:59:45 +0530

    sign up functionality done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Homepage } from './pages/Homepage';
-import {ChatPage} from './pages/Chatpage';
+import { Chartpage } from './pages/Chartpage';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Homepage />} />
-        <Route path='/chats' element={<ChatPage />} />
+        <Route path='/chats' element={<Chartpage />} />
       </Routes>
     </div>
   );
